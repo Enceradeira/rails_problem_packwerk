8ccc009ed0c281a71e62ceaeeb5f10e0d32b2199
Jorg Jenni <jorg.jenni@jennius.co.uk>
2022-03-19 09:14:21 +0000

    add depedency violations (configure dependency & privacy violation)

--- app/models/accounting/accounts.rb
+++ app/models/accounting/accounts.rb
@@ -2,7 +2,10 @@ module Accounting
   module Accounts
     class << self
       def find_account(referenz)
+        # following is a dependency violation
         Debtors::DebtorsService.known_debtor?(referenz)
+        # following is a dependency  & privacy violation
+        Debtors::Enquiries.find_worst_debtors
       end
     end
   end
